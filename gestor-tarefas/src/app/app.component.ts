a563fb20eeb3b497e500296d4987374749a0a59e
Sandra Bastos <sandra@vermillion.net.br>
2024-08-12 14:52:58 -0300

    fixes toolbar and sidenav

--- gestor-tarefas/src/app/app.component.ts
+++ gestor-tarefas/src/app/app.component.ts
@@ -1,19 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { MatDrawerContainer, MatDrawer,  } from "@angular/material/sidenav";
-import { MatButtonModule,  } from '@angular/material/button';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatNavList, MatListItem } from '@angular/material/list';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [MatDrawerContainer, MatDrawer, MatButtonModule, MatToolbarModule, MatIconModule, MatNavList, MatListItem],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
+  imports: [ NavigationComponent],
+  standalone: true
 })
 export class AppComponent {
-  title = 'gestor-tarefas';
-  showFiller = false;
+  title = 'Angular Material Toolbar and Side Menu';
 }
